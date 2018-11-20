30934ca470bd42704b7a9e2a8b968cdcdd47841e
the4kgamer <39601293+the4kgamer@users.noreply.github.com>
2018-11-21 06:01:42 +1100

    Update adminkickban.php
    same thing, don't want to worry anyone

--- adminkickban.php
+++ adminkickban.php
@@ -46,7 +46,7 @@ if (isset($_POST['delete'])) {
     $serverinfo = array
        (
 
-        array("0","SinisterRPChicagoLaw","185.249.196.181","32066","6f96d22e18"),
+        array("0","SinisterRPChicagoLaw","185.249.196.181","32066",""),
 	    
 
            
