57343986435d540b4759fb5d58b0f9676e227bc0
the4kgamer <39601293+the4kgamer@users.noreply.github.com>
2018-11-21 06:01:10 +1100

    Update testrcon.php
    rcon password has been changed, but i'd rather not worry anyone

--- testrcon.php
+++ testrcon.php
@@ -6,7 +6,7 @@ session_start();
 $serverinfo = array
        (
 
-        array("0","SinisterRPChicagoLaw","185.249.196.181","32066","6f96d22e18"),
+        array("0","SinisterRPChicagoLaw","185.249.196.181","32066",""),
 	    
 
            
